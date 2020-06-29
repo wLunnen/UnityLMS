4fe2d8ee62db2003d22716ce32678de003b02c5b
Wesley Lunnen <wesley.lunnen@gmail.com>
2020-06-29 13:47:17 -0500

    commit

---
+++ UnityLMS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UnityLMS.MvcApplication" Language="C#" %>
---
+++ UnityLMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UnityLMS.Startup))]
+namespace UnityLMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
